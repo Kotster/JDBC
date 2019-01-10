a2085befe72827a789feeff276b191a9c616386c
Kotster <der.mikha@yandex.ru>
2019-01-10 21:52:19 +0300

    test

---
+++ start.bat
@@ -0,0 +1,3 @@
+javac -d bin -sourcepath src -classpath C:\Users\Admin\Desktop\testspring\src\spring-framework-5.1.3.RELEASE\libs\spring-context-5.1.3.RELEASE.jar;C:\Users\Admin\Desktop\testspring\src\spring-framework-5.1.3.RELEASE\libs\spring-core-5.1.3.RELEASE.jar;C:\Users\Admin\Desktop\testspring\src\spring-framework-5.1.3.RELEASE\libs\spring-beans-5.1.3.RELEASE.jar src\Main.java
+java -classpath bin Main
+pause
